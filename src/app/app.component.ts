9089b7355ce9feec19d3397b96b101f3043be703
jozimarback <jozimar.back@evosix.com.br>
2018-01-22 22:31:04 -0200

    Criado formad e calcular e exibir quantidade de agua para beber

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'water reminder';
+  quantidadeAgua:number = 0;
+  constructor(private formBuilder:FormBuilder) {
+    
+  }
+  formulario:FormGroup;
+  ngOnInit() {
+    this.formulario = this.formBuilder.group({
+      pesoGramas:this.formBuilder.control('',[Validators.required,Validators.minLength(2)])
+    });
+    
+  }
+
+  onSubmit(): void {
+    this.quantidadeAgua = this.formulario.value.pesoGramas * 35;
+  }
 }
